3241535ab47f8b67f9f50e636d2cc4bbee574768
tree <tree@hidden.cz>
2022-04-07 01:50:28 +0200

    Update

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -14,11 +14,11 @@
 
 <header class="relative" style="background-color: #32375C;">
   <!--		<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
-	<nav class="relative mx-auto lg:px-6 px-4 py-6 max-w-6xl text-center">
+	<nav class="relative mx-auto lg:px-6 px-4 pt-4 sm:pt-6 sm:pb-6 pb-2 max-w-6xl text-center">
     <div class="">
       <div class="lg:flex lg:flex-wrap lg:space-x-10">
-        <div class="block justify-start lg:flex-1 my-auto text-center pb-4 lg:pb-0">
-          <div class="w-44 lg:w-32 inline-block lg:block"><a href="/" on:click={logoClick}><img src="/img/logo-white.svg" class="w-full" alt="UTXO.22" /></a></div>
+        <div class="block justify-start lg:flex-1 my-auto text-center pb-3 lg:pb-0">
+          <div class="w-36 lg:w-32 inline-block lg:block"><a href="/" on:click={logoClick}><img src="/img/logo-white.svg" class="w-full" alt="UTXO.22" /></a></div>
         </div>
         <div class="flex lg:space-x-10 uppercase text-sm font-bold text-white">
           <a sveltekit:prefetch href="/" class="lg:w-auto w-1/3 m-auto hover:text-[#E16A61]" class:text-blue-400={$page.url.pathname === '/'}>O konferenci</a>
