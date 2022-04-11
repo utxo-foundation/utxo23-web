953b3d3ff69ca1b8c14fd4ec2f19374a6c00e055
tree <tree@hidden.cz>
2022-04-11 18:22:08 +0200

    Update

--- src/routes/program.svelte
+++ src/routes/program.svelte
@@ -15,7 +15,7 @@
 
 <section class="relative mx-auto py-6 sm:py-10 px-6 max-w-6xl text-blue-web">
   <h1 class="uppercase text-2xl font-bold">Program</h1>
-  <div class="mt-2 text-sm">Program stále připravujeme. Jeho konečná podoba bude zveřejněna par týdnů před konferencí.</div>
+  <div class="mt-2 text-sm">Program stále připravujeme. Jeho konečná podoba bude zveřejněna pár týdnů před konferencí.</div>
 
   <div class="mt-6">
     {#each $bundle.spec.events.filter(e => !e.parent) as e}
