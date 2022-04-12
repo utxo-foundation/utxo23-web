9dc4c021d1db81225bba2c6ebbed2fd7e3ccbc44
tree <tree@hidden.cz>
2022-04-13 00:12:03 +0200

    update

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -22,7 +22,7 @@
           Otevřená komunitní kryptoměnová konference
         </div>
         <div class="mt-4">
-          <a href={$bundle.links.docs} class="hover:text-red-500"
+          <a href={$bundle.links.docs} class="hover:text-red-500" target="_blank"
             ><i class="fas fa-book" />&nbsp; Dokumentace</a
           >
         </div>
