9f38d85acace0d0059cfd1caa9914155b2e71fca
tree <tree@hidden.cz>
2022-04-28 01:06:11 +0200

    Add sitemaps generator

---
+++ src/lib/sitemaps.js
@@ -0,0 +1,40 @@
+
+import { readFileSync, writeFileSync } from 'fs';
+
+const bundle = JSON.parse(readFileSync('./src/lib/bundle.json'))
+
+function makeItem ({ url, lastmod }) {
+  if (!lastmod) {
+    lastmod = new Date().toISOString()
+  }
+  return `  <url>
+    <loc>${url}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </url>`
+}
+
+const items = []
+const link = p => items.push(makeItem({ url: `https://utxo.cz/${p}` }))
+
+// add links
+link('program')
+link('vstupenky')
+
+for (const x of bundle.spec.speakers) {
+  link(`prednasejici?id=${x.id}`)
+}
+for (const x of bundle.spec.events) {
+  link(`udalosti?id=${x.id}`)
+}
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${items.join("\n")}
+</urlset>`
+
+//console.log(sitemap)
+const outputFn = './build/sitemap.xml'
+writeFileSync(outputFn, sitemap)
+
+console.log(`Sitemap writed: ${outputFn}`)
+
