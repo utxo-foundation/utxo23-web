891b9dc1521efe7b6d3f78f6612fde77c7734626
tree <tree@hidden.cz>
2022-05-24 21:50:58 +0200

    Update

--- src/lib/Avatar.svelte
+++ src/lib/Avatar.svelte
@@ -71,10 +71,10 @@
 {/if}
 
 {#if size === "big"}
-  <div class="w-64 text-center pb-4 m-auto">
+  <div class="w-64 h-64 text-center pb-4 m-auto">
     <img
       src={currentImg}
-      class="w-64 rounded-full m-auto shadow-xl"
+      class="w-64 h-64 rounded-full m-auto shadow-xl"
       alt={speaker.name}
       on:mouseover={mouseOver}
       on:mouseleave={mouseLeave}
@@ -90,7 +90,7 @@
       on:mouseleave={mouseLeave}
       ><img
         src={currentImg}
-        class="w-36 sm:w-40 rounded-full m-auto shadow-xl"
+        class="w-36 sm:w-40 h-36 sm:h-40 rounded-full m-auto shadow-xl"
         alt={speaker.name}
       /></a
     >
@@ -114,16 +114,16 @@
 {/if}
 
 {#if size === "small"}
-  <div class="w-16 text-center">
-    <img src={currentImg} class="w-16 rounded-full m-auto" alt={speaker.name} />
+  <div class="w-16 h-16 text-center">
+    <img src={currentImg} class="w-16 h-16 rounded-full m-auto" alt={speaker.name} />
   </div>
 {/if}
 
 {#if size === "semi-small"}
-  <div class="w-10 text-center">
+  <div class="w-10 h-10 text-center">
     <img
       src={currentImg}
-      class="w-10 rounded-full m-auto shadow-md"
+      class="w-10 h-10 rounded-full m-auto shadow-md"
       alt={speaker.name}
     />
   </div>
