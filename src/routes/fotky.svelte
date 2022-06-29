99d16b367afb6f7f3298c6ef9f5775002609b499
tree <tree@hidden.cz>
2022-06-29 17:51:38 +0200

    Update

--- src/routes/fotky.svelte
+++ src/routes/fotky.svelte
@@ -6,6 +6,7 @@
 
 <section class="relative mx-auto py-6 sm:py-10 px-6 max-w-6xl text-blue-web">
   <h1 class="uppercase text-2xl font-bold mb-4"><a href="https://photos.app.goo.gl/8ogj2Y3cdUGwMAc4A">Fotky</a></h1>
+  <div class="mb-4"><a href="https://photos.app.goo.gl/8ogj2Y3cdUGwMAc4A" target="_blank" class="underline hover:no-underline">Zobrazit všechny fotky</a></div>
 
   <div class="aspect-video">
     <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
