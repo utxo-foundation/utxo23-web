387ebf34934b873e2ec5c4abc6766ebeed713eb4
burningtree <burningtree@users.noreply.github.com>
2023-01-20 00:30:10 +0000

    deploy: 5681df67b2a8c691330fc1f151d040d470dba234

---
+++ _app/chunks/api-2f363070.js
@@ -0,0 +1,53 @@
+import{b as s}from"./stores-ca1bbb18.js";const r="utxo23",i="UTXO.23",c="UTXO",p="Otev\u0159en\xE1 komunitn\xED kryptom\u011Bnov\xE1 konference",u=["2023-06-02","2023-06-04"],l="Czech Republic",d={web:"https://utxo.cz",docs:"https://docs.utxo.cz",twitter:"https://twitter.com/utxoprague",telegram:"https://t.me/utxoprague",fbpage:"https://www.facebook.com/utxoprague",instagram:"https://www.instagram.com/utxoprague/",youtube:"https://www.youtube.com/c/UTXOPrague"},m=1,k={speakers:[{id:"petr-lukac",name:"Petr Luk\xE1\u010D",twitter:"RuzovejSlon",bio:"Novin\xE1\u0159, podcaster",orgs:`Podcast [Krypto Space](https://www.kryptospace.cz/)
+`,tracks:["spolecnost"],country:"cz",photos:["web:png","twitter:jpg"]},{id:"tereza-starostova",name:"Tereza Starostov\xE1",twitter:"terkastarostova",orgs:`Spoluzakladatelka [Holky v Kryptu](https://holkyvkryptu.cz/), p\u0159edsedkyn\u011B [UTXO Foundation](httsp://utxo.foundation)
+`,tracks:["zaklady","spolecnost"],country:"cz",photos:["twitter:jpg"]},{id:"tree",name:"tree",twitter:"treecz",bio:"Libertari\xE1n, propag\xE1tor Web3 a DAOs",orgs:`Zakladatel [Gwei.cz](https://gwei.cz). Organiz\xE1tor [ETHBrno](https://ethbrno.cz/), [UTXO.23](https://utxo.cz) a dal\u0161\xEDch akc\xED
+`,country:"cz",tracks:["eth","defi","dao"],desc:`Founder of @gweicz | \u{1F984} #Ethereum #DeFi #DAO enthusiast \u{1F987}\u{1F50A} | \u2708\uFE0F #travel maniac | \u26A1 riding #electricunicycle | \u{1F1E8}\u{1F1FF}\u{1F1EC}\u{1F1E7} lang | \u{1F7E1}\u26AB | \u{1F1FA}\u{1F1E6} \u0421\u043B\u0430\u0432\u0430 \u0423\u043A\u0440\u0430\u0457\u043D\u0456!
+`,photos:["twitter:jpg"]}],tracks:[{id:"zaklady",name:"Z\xE1klady kryptom\u011Bn",shortname:"Z\xE1klady",examples:`* principy otev\u0159en\xFDch projekt\u016F (blockchain\u016F)
+* z\xE1klady bezpe\u010Dnosti a soukrom\xED
+* pen\u011B\u017Eenky - SW, HW
+* z\xE1klady obchodov\xE1n\xED - DCA
+`},{id:"btc",name:"Bitcoin",examples:`* Bitcoin jako pen\xEDze
+* Lightning Network - adopce, implementace
+* Taproot
+* Bitcoin mining
+* smart-kontrakty na bitcoinu (RGB)
+`},{id:"eth",name:"Ethereum a smart-kontrakty obecn\u011B",shortname:"Ethereum",examples:`* Ethereum 2.0
+* Layer2 - rollupy
+* (EVM) Sidechainy
+* v\xFDvoj smart-kontrakt\u016F (solidity)
+* MEV - Miner Extractable Value
+`},{id:"defi",name:"Decentralizovan\xE9 finance (DeFi)",shortname:"DeFi",examples:`* stablecoiny
+* lending protokoly
+* DEX - decentralizovan\xE9 burzy
+* predik\u010Dn\xED trhy
+* futures/options kontrakty
+* tokenizace
+`},{id:"dao",name:"Decentralizovan\xE9 organizace (DAO)",shortname:"DAOs",examples:`* budoucnost organizac\xED
+* showcase jednotliv\xFDch DAOs
+`},{id:"nft",name:"NFTs a Metaverse",shortname:"NFTs/Metaverse",examples:`* NFT um\u011Bn\xED
+* NFT collectibles
+* gaming
+* Metaverse
+`},{id:"alty",name:"Experiment\xE1ln\xED L1 blockchainy a dal\u0161\xED altcoiny",shortname:"Alternativn\xED L1",examples:`* Polkadot
+* Cardano
+* Cosmos (Secret Network..)
+* Solana
+* NEAR
+* Monero
+* ...
+`},{id:"regulace",name:"Regulace a fiat rampy",shortname:"Regulace",examples:`* legislativa
+* sm\u011Bn\xE1rny a burzy
+* darov\xE1n\xED kryptom\u011Bn
+* insitucion\xE1ln\xED pen\xEDze v kryptom\u011Bn\xE1ch
+`},{id:"spolecnost",name:"Krypto-spole\u010Dnost",shortname:"Spole\u010Dnost",examples:`* odluka pen\u011Bz od st\xE1tu
+* kryptoanarchismus, anarchokapitalismus, meritokracie
+* p\u0159edstaven\xED lok\xE1ln\xEDch krypto-komunit
+* fenom\xE9n maximalismu
+`},{id:"other",name:"Ostatn\xED",hidden:!0}],"event-types":[{id:"talk",name:"P\u0159edn\xE1\u0161ka"},{id:"workshop",name:"Workshop"},{id:"panel",name:"Panelov\xE1 debata",shortname:"Panelovka"},{id:"lightning-series",name:"P\xE1smo bleskov\xFDch p\u0159edn\xE1\u0161ek",shortname:"Bleskovky",url:"https://docs.utxo.cz/hlavni-program/formaty-programu#bleskova-prednaska-lightning-talk"},{id:"lightning",name:"Bleskov\xE1 p\u0159edn\xE1\u0161ka",shortname:"Bleskovka",hidden:!0},{id:"campfire",name:"Campfire"},{id:"other",name:"Ostatn\xED"}],faqs:[{question:"Jak z\xEDsk\xE1m vstupenku?",answer:`TODO
+`},{question:"Pro koho je konference ur\u010Dena?",answer:`Pro v\u0161echny kryptom\u011Bnov\xE9 nad\u0161ence. Konference vznikla tak\xE9 jako neutr\xE1ln\xED m\xEDsto pro dialog v r\xE1mci cel\xE9 lok\xE1ln\xED krypto-komunity. Ka\u017Ed\xFD n\u011B\u010Demu fand\xEDme, ale poj\u010Fme se od toho alespo\u0148 chv\xEDli odprostit a poslechnout si i my\u0161lenky protistrany, nebo diskutovat.
+`},{question:"Budou dostupn\xE9 z\xE1znamy p\u0159edn\xE1\u0161ek?",answer:`Ano, v\u0161echny p\u0159edn\xE1\u0161ky z hlavn\xEDch 4 s\xE1l\u016F (P1-P4) budou po konferenci dostupn\xE9 zdarma na UTXO.TV nebo na\u0161em [YouTube kan\xE1lu](https://www.youtube.com/channel/UCXLBCW1pstQPQf-4zRESepA).
+`},{question:'Co to znamen\xE1 "UTXO"?',answer:`"UTXO" znamen\xE1 "*Unspent transaction output*", co\u017E by se dalo p\u0159elo\u017Eit jako "neutracen\xFD v\xFDstup transakce". Jedn\xE1 se o [z\xE1kladn\xED mechanismus fungov\xE1n\xED bitcoinu](https://www.alza.cz/transakcni-poplatky-a-minimalni-velikost-utxo#co-je-utxo).
+
+> "_UTXO je jedin\xFD zp\u016Fsob, jak\xFDm v\xE1\u0161 bitcoin skute\u010Dn\u011B "existuje". Ka\u017Ed\xE1 transakce bere p\u0159edchoz\xED neutracen\xE9 v\xFDstupy, pou\u017E\xEDv\xE1 je jako vstupy a ve v\xFDsledku vytv\xE1\u0159\xED nov\xE9 v\xFDstupy. Ka\u017Ed\xFD konec je nov\xFDm za\u010D\xE1tkem. Bitcoin je kr\xE1sn\xFD._" - [@SatsJoseph](https://twitter.com/SatsJoseph/status/1370329486059843588)
+`},{question:"Budou p\u0159edn\xE1\u0161ky vys\xEDlan\xE9 \u017Eiv\u011B?",answer:`Ano, pl\xE1nujeme bohat\xFD program i pro n\xE1v\u0161t\u011Bvn\xEDky online - \u017Eiv\xE9 p\u0159enosy ze v\u0161ech p\u0159edn\xE1\u0161kov\xFDch s\xE1l\u016F, rozhovory s n\xE1v\u0161t\u011Bvn\xEDky a dal\u0161\xED obsah. V\u0161e bude zdarma dostupn\xE9 na dom\xE9n\u011B UTXO.TV (a [YouTube](https://www.youtube.com/channel/UCXLBCW1pstQPQf-4zRESepA)).
+`}],partners:[{id:"gweicz",name:"Gwei.cz",type:"community",twitter:"gweicz",desc:"Ethereum/DeFi/Web3 komunita",photos:["twitter:jpg"]},{id:"holky-v-kryptu",name:"Holky v kryptu",type:"community",twitter:"holkyvkryptu",desc:"Komunita pro za\u010D\xEDnaj\xEDc\xED a m\xEDrn\u011B pokro\u010Dil\xE9 u\u017Eivatele",photos:["twitter:jpg"]}],team:{persons:{tereza:{name:"Tereza Starostov\xE1",twitter:"terkastarostova"},tree:{name:"Tree",twitter:"treecz"}},teams:{}}},h={counts:{speakers:3,tracks:10,"event-types":7,faqs:5,partners:2}},y="2023-01-20T00:13:09.989Z";var v={id:r,name:i,shortname:c,description:p,dates:u,country:l,links:d,schemaVersion:m,spec:k,stats:h,time:y};class z{constructor(){this.bundle=null}async load(){await this.loadBundle()}async loadBundle(a=!1){if(!this.bundle){if(a)this.bundle=v;else{const e=await fetch("https://spec.utxo.cz/23/bundle.json");this.bundle=await e.json()}s.set(this.bundle);for(const e of this.bundle.spec.events)if(e.speakersInfo=e.speakers.map(n=>{const t=this.bundle.spec.speakers.find(o=>o.id===n);return t?{name:t.name,nickname:t.nickname,bio:t.bio,orgs:t.orgs,description:t.desc}:null}),this.bundle.schedule){const n=this.bundle.schedule.find(t=>t.event===e.id);n&&(e.scheduleId="@"+n.id)}}return this.bundle}async apiCall(a,e={method:"get"},n=null){n&&(e.headers={"Content-Type":"application/json"},e.body=JSON.stringify(n));const t=await fetch(`https://api.utxo.cz/${a}`,e);return t.status!==200?null:t.json()}async submitOrder(a){return(await fetch("https://api.utxo.cz/submitOrder",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(a)})).json()}}const g=new z;export{g as a};
---
+++ _app/chunks/orders-851f93e7.js
@@ -0,0 +1,1 @@
+import{a as o,c as e}from"./stores-ca1bbb18.js";import{a}from"./api-2f363070.js";async function d(t){if(t.orders&&t.orders.length>0||t.tickets&&t.tickets.length>0){const r=await a.apiCall("orders",{method:"POST"},{orders:t.orders,tickets:t.tickets});o.update(s=>(s.orders=r.orders,s.tickets=r.tickets,s))}}async function n(){e.set(await a.apiCall("status"))}export{d as a,n as l};
