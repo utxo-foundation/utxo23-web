42239f45b9043c100d743239088aa1fb8b980bf9
tree <tree@hidden.cz>
2023-02-04 11:33:40 +0100

    Update deps, fix install

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,6 @@ const entries = []
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
     prerender: {
       crawl: true,
       entries: [
