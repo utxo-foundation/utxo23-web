8e068a966d1cd516ff214019baa82af94cfc8426
Antonin Jilek <antonin.jilek@webatelier.cz>
2023-02-13 17:28:54 +0100

    make buildable

--- src/routes/mapa/+page.svelte
+++ src/routes/mapa/+page.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  throw new Error("@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)");
+  // throw new Error("@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)");
 
   // export const prerender = true;
 
