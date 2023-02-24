311a7322f510aeb8456351e0c8a0c9f0ffda986c
Antonin Jilek <antonin.jilek@webatelier.cz>
2023-02-24 21:12:44 +0100

    give prerenderer all links to lide

---
+++ src/routes/prerender-links/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { bundle, userData, apiStatus } from "$lib/stores.js";
+    import Avatar from "$lib/Avatar.svelte";
+    import SvelteMarkdown from "svelte-markdown";
+    import Link from "$lib/Link.svelte";
+    import SvelteTooltip from "$lib/SvelteTooltip.svelte";
+    import Transition from "$lib/Transition.svelte";
+    const renderers = { link: Link };
+  
+    export let data;
+  
+  </script>
+  
+  <svelte:head>
+    <title>UTXO.23 {data.bundle ? "- " + data.bundle.description : ""}</title>
+  </svelte:head>
+  
+  {#if data.bundle}
+    <ul>
+      {#each data.bundle.spec.speakers as speaker}
+        <li><a href="/lide/{speaker.id}" class="href">{speaker.name}</a></li>
+      {/each}
+    </ul>
+  {/if}
