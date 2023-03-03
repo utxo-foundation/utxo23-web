29ee025f8909e5e95d5678c0ec09641959b974ed
cryptonda <cryptonda@users.noreply.github.com>
2023-03-03 20:48:37 +0000

    deploy: add5daee19917862959869d62472ca8c1091916c

--- _app/immutable/chunks/4-1149c851.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/fotky/_page.svelte-92d51892.js";export{t as component};
---
+++ _app/immutable/chunks/4-30357fdf.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/fotky/_page.svelte-f46e8c70.js";export{t as component};
