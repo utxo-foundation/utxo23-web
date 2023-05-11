9ba104a65e6e33cdc84e4e290071c80fed7dea14
cryptonda <cryptonda@users.noreply.github.com>
2023-05-11 13:17:39 +0000

    deploy: 49b851c67d0e04302aca07dafa8e97f5aff500a6

--- _app/immutable/chunks/2-74732427.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c0d9cf2b.js";export{t as component};
---
+++ _app/immutable/chunks/2-d3a44c29.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e82f3923.js";export{t as component};
